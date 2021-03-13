2e9c391d33ffa17f0124ee52979db5bc1fcde979
abdalluhmostafa <51482871+abdalluhmostafa@users.noreply.github.com>
2021-03-13 14:37:45 +0200

    Update sshalert.sh

--- sshalert.sh
+++ sshalert.sh
@@ -4,6 +4,7 @@
 #Description    :send alert mail when anyone login via ssh
 #Author         :Abdalluh Mostafa
 #Email          :abdalluh.mostafa@gmail.com
+#Githun         : https://github.com/abdalluhmostafa/
 ### 2021 	
 # Please change email to your email inside alert file 
 #######################################################################################
